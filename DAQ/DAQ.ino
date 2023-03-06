8d3f5c38da37f271072fe6721541fcb8fb5372b4
VimFreeman <github@freiman.cc>
2023-03-06 10:11:42 -0500

    Clean up the code

--- DAQ/DAQ.ino
+++ DAQ/DAQ.ino
@@ -5,7 +5,7 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define CCS811_ADDR 0x5A
+#define CCS811_ADDR 0x5A // CCS811 I2C ADDRESS
 #define DHT_PIN 32
 #define DHT_TYPE DHT22
 
@@ -14,17 +14,20 @@ DHT_Unified myDHT(DHT_PIN, DHT_TYPE);
 
 BluetoothSerial SerialBT;
 
-uint8_t voc = 0;
-uint8_t co2 = 0;
+uint16_t voc = 0;
+uint16_t co2 = 0;
 float temp = 0;
 float hum = 0;
+
+void getCCS();
+void getDHT();
 
 void setup()
 {
   Serial.begin(115200);
   SerialBT.begin("PAQ-3000");
 
-  Wire.begin();
+  Wire.begin(); // Enable I2C Peripheral
 
   if (myCCS.begin() == false)
   {
@@ -41,66 +44,59 @@ void loop()
 {
   if (myCCS.dataAvailable())
   {
-    myCCS.readAlgorithmResults();
-
-    co2 = myCCS.getCO2();
-    voc = myCCS.getTVOC();
-
-    Serial.print("CO2[");
-    //Returns calculated CO2 reading
-    Serial.print(co2);
-    Serial.print("] tVOC[");
-    //Returns calculated TVOC reading
-    Serial.print(voc);
-    Serial.print("]");
-    //Display the time since program start
-    Serial.println();
+    getCCS();
+    getDHT(); // we put it with myCCS so as not to spam the console
   }
 
-  sensors_event_t event;
-  myDHT.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else {
-    temp = event.temperature;
-    Serial.print(F("Temperature: "));
-    Serial.print(temp);
-    Serial.println(F("°C"));
-  }
-  
-  myDHT.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else {
-    hum = event.relative_humidity;
-    Serial.print(F("Humidity: "));
-    Serial.print(hum);
-    Serial.println(F("%"));
-  }  
-
+  // handle bluetooth requests
   if (SerialBT.available()) {
     uint8_t cmd = SerialBT.read();
     switch(cmd) {
       case 48:
-        SerialBT.write(&co2, 2);
-        SerialBT.write(10);
-        SerialBT.write(&voc, 2);
-        SerialBT.write(10);
-        SerialBT.write(10);
-        break;
-      case 49:
-        SerialBT.write((uint8_t)temp);
-        SerialBT.write(10);
-        SerialBT.write((uint8_t)hum);
-        SerialBT.write(10);
-        SerialBT.write(10);
+        SerialBT.write((uint8_t*)&co2, 2);
+        SerialBT.write((uint8_t*)&voc, 2);
+        SerialBT.write((uint8_t*)&temp, 4);
+        SerialBT.write((uint8_t*)&hum, 4);
         break;
       default:
         break;
     }
   }
 
-  delay(1000); //Don't spam the I2C bus
+  delay(10); //Don't spam the I2C bus
+}
+
+void getCCS() {
+  myCCS.readAlgorithmResults();
+
+  co2 = myCCS.getCO2();
+  voc = myCCS.getTVOC();
+
+  // Serial port sensor debugging
+  Serial.print("CO2: ");
+  Serial.println(co2);
+  Serial.print("tVOC: ");
+  Serial.println(voc);
+  // ----------------------------
+}
+
+void getDHT() {
+  sensors_event_t tempEvent;
+  sensors_event_t humEvent;
+
+  myDHT.temperature().getEvent(&tempEvent);
+  myDHT.humidity().getEvent(&humEvent);
+
+  hum = humEvent.relative_humidity;
+  temp = tempEvent.temperature;
+
+  // Serial port sensor debugging
+  Serial.print(F("Temperature: "));
+  Serial.print(temp);
+  Serial.println(F("°C"));
+
+  Serial.print(F("Humidity: "));
+  Serial.print(hum);
+  Serial.println(F("%"));
+  //------------------------------
 }
